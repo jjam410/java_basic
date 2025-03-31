6f5aea20e69ec004d607660389d6bebab53fa391
김수환 <tnghks5859@naver.com>
2025-03-31 22:52:10 +0900

    변수 및 형변환 기초

---
+++ exam01/src/exam01/HelloJava.java
@@ -0,0 +1,33 @@
+package exam01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		// 정수형 타입 선언
+		int num01 = 10;
+		byte byNum = 20;
+		
+		// 실수
+		double doubleNum = 30.3111;
+		float flNum = 33.12f;
+		
+		// 문자
+		char ch = 'a';
+		
+		// 문자열
+		String str = "날씨";
+		
+		// 논리형
+		boolean b = false;
+		
+		// 문장이 끝나고 줄이 바뀌는 명령
+		System.out.println(num01);
+		System.out.println(byNum);
+		System.out.print(doubleNum + "\n");
+		System.out.println(ch);
+		System.out.println(str);
+		System.out.println(b);
+
+	}
+}
