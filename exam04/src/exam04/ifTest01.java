c723ed27d8190118f4cdca9aa0310cfcd907aba3
김수환 <tnghks5859@naver.com>
2025-04-23 18:51:26 +0900

    조건문 if문

---
+++ exam04/src/exam04/ifTest01.java
@@ -0,0 +1,22 @@
+package exam04;
+
+public class ifTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int score = 80;
+		
+		// 점수가 60점 이상이면 합격
+		
+		if (score >= 60) {
+			// 참일 때 실행
+			System.out.println("합격!");
+		} else {
+			// 결과가 거짓
+			System.out.println("불합격");
+		}
+
+	}
+
+}
